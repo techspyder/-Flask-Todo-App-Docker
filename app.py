bef344abe3704dc0d7fed1090f3e8c519a96fa4a
Sandeep Kumar Swain <139670127+techspyder@users.noreply.github.com>
2023-08-24 19:47:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Welcome to Flask Todo App ! Hello Jenkins"
+    return "Welcome to Flask Todo App ! Hello Jenkins!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
